2dde0630a041f38b0a0b5c9d7dbd7b2e34d586f2
Salman <salmannazeer477@gmail.com>
2026-01-05 14:22:20 +0530

    Adjust styles

--- src/Board.jsx
+++ src/Board.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 export default function Board({switchCurrentPlayer, currentPlayer}){
 
     const [board, setBoard] = useState([
-                                        ' ', ' ', ' ',
-                                        ' ', ' ', ' ',
-                                        ' ', ' ', ' ',
+                                        '.', '.', '.',
+                                        '.', '.', '.',
+                                        '.', '.', '.',
                                     ])
 
     function updateBox(i, currentPlayer){
