a9f06f578c3d1a49a2c2dcd799710c02e915075e
Salman <salmannazeer477@gmail.com>
2026-01-05 13:43:51 +0530

    Created Board component

---
+++ src/Board.jsx
@@ -0,0 +1,21 @@
+export default function Board(){
+    return (
+        <>
+            <div className="row">
+                <button className="box">1</button>
+                <button className="box">2</button>
+                <button className="box">3</button>
+            </div>
+            <div className="row">
+                <button className="box">1</button>
+                <button className="box">2</button>
+                <button className="box">3</button>
+            </div>
+            <div className="row">
+                <button className="box">1</button>
+                <button className="box">2</button>
+                <button className="box">3</button>
+            </div>
+        </>
+    )
+}
