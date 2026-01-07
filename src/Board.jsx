df128589883e4dccc524e261f1891a692d4e5cab
Salman Nazeer <salmannazeer477@gmail.com>
2026-01-07 15:12:19 +0530

    Added code to detect victor

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,9 +1,20 @@
 import { useState, useEffect } from 'react'
 export default function Board({ switchCurrentPlayer, currentPlayer, setVictor }) {
-
     const [board, setBoard] = useState(Array(9).fill(null))
 
-
+    if(board[0] || board[4] || board[8]){
+        // Horizontal
+        if(board[0] === board[1] && board[1] === board[2]) setVictor(board[0])
+        if(board[3] === board[4] && board[4] === board[5]) setVictor(board[3])
+        if(board[6] === board[7] && board[7] === board[8]) setVictor(board[6])
+        // Vertical
+        if(board[0] === board[3] && board[3] === board[6]) setVictor(board[0])
+        if(board[1] === board[4] && board[4] === board[7]) setVictor(board[1])
+        if(board[2] === board[5] && board[5] === board[8]) setVictor(board[2])
+        // Diagonal
+        if(board[0] === board[4] && board[4] === board[8]) setVictor(board[0])
+        if(board[2] === board[4] && board[4] === board[6]) setVictor(board[2])
+    }
     function updateBox(i, currentPlayer) {
         if (board[i] !== null) return
         setBoard(board => {
